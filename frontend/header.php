999de041f51122b7bceac8cd22119aa619eda681
devshreeeee22 <b230048@nitsikkim.ac.in>
2025-01-01 13:20:29 +0530

    Add files via upload

---
+++ frontend/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../main.css">
+</head>
+<body>
+    <header class="header">
+        <nav>
+                <h1 class="logo" href="./home.html">TRIPCRAFT</h1>
+                <ul>
+                    <li><a href="./home.html">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="./packages.php">Packages</a></li>
+                    <li><a href="#book">Book</a></li>
+                    <!-- <li><a href="#Contact">Contact</a></li> -->
+                </ul>
+                
+
+                </div>
+                <i class="bi bi-three-dots"></i>
+        </nav>
+        
+    </header>   
+</body>
+</html>
