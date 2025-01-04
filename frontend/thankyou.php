9948d42174805c5759cd0c87d572807cc133e085
devshreeeee22 <b230048@nitsikkim.ac.in>
2025-01-04 10:40:03 +0530

    Add files via upload

---
+++ frontend/thankyou.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>TripCraft</title>
+        <link href="../main.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&family=Dancing+Script:wght@400..700&family=Exo+2:ital,wght@0,100..900;1,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playwrite+MX+Guides&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+        
+        <style>
+            .thankyou-content {
+                /* border: 1px solid black; */
+                margin-top: 20%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                text-align: center;
+                
+            }
+
+            .thankyou-content h1 {
+                font-family: 'ubuntu';
+                font-weight: 500;
+                font-style: normal;
+                font-size: 60px;
+                color: #dc48d3;
+            }
+            .thankyou-content p {
+                margin-top: 0;
+                font-size: 25px;
+                font-weight: 400;
+            }
+        </style>
+    </head>
+    <body>
+        <header class="header">
+            <nav>
+                <h1 class="logo" href="./home.html">TRIPCRAFT</h1>
+                <ul>
+                    <li><a href="./home.html">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="./packages.php">Packages</a></li>
+                    <li><a href="#book">Book</a></li>
+                    <!-- <li><a href="#Contact">Contact</a></li> -->
+                </ul>
+                
+
+                </div>
+                <i class="bi bi-three-dots"></i>
+            </nav>
+            <div class="thankyou-content">
+                <h1>Thank you for your enquiry.</h1>
+                <p>
+                Your message has been sent successfully, our consultants will get back to you as soon as possible</p>
+            </div>
+        </header>
+        
+    </body>
+</html>
